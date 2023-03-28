46222be227deadbec6ce9045b46e5e3c3809bcf1
iLeepop <zihao02325@gmail.com>
2023-03-28 22:04:36 +0800

    第三次提交

--- JS/JS案例/TB轮播图/tblbt.js
+++ JS/JS案例/TB轮播图/tblbt.js
@@ -2,20 +2,25 @@ window.addEventListener('load', function () {
     let main = document.querySelector('.main')
     let ar = document.querySelector('.arrow-r')
     let al = document.querySelector('.arrow-l')
-    main.addEventListener('mouseenter', function() {
-        ar.style.display = 'block'
-        al.style.display = 'block'
-    })
-    main.addEventListener('mouseleave', function() {
-        ar.style.display = 'none'
-        al.style.display = 'none'
-    })
     let lbt_ul = main.querySelector('.img-roll')
     let olist = main.querySelector('.olist')
+
     let imglist = lbt_ul.children
     for (let i = 0; i < imglist.length; i++) {
         let li = document.createElement('li')
         olist.appendChild(li)
     }
     olist.children[0].className = 'current-ol'
+
+    main.addEventListener('mouseenter', function() {
+        ar.style.display = 'block'
+        al.style.display = 'block'
+        olist.style.display = 'block'
+    })
+    main.addEventListener('mouseleave', function() {
+        ar.style.display = 'none'
+        al.style.display = 'none'
+        olist.style.display = 'none'
+
+    })
 })
